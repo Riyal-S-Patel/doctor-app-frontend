aea35677df92d932c0ea88ef59c7ee18f3d8de00
Riyal <riyalpatel2995@gmail.com>
2024-01-09 20:29:06 +0530

    logout button implemented

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,23 @@
-import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Doctor } from '../model/doctor';
 import { Hospital } from '../model/hospital';
 import { FormsModule } from '@angular/forms';
 import { DoctorService } from '../service/doctor.service';
 import { HttpClientModule } from '@angular/common/http';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, HttpClientModule],
+  imports: [FormsModule, HttpClientModule, NgFor, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  providers:[DoctorService]
+  providers: [DoctorService]
 })
-export class HomeComponent implements AfterViewInit {
-
-
-  imageArray = ['a','b','c','d','e','f'];
-  searchText="";
-  doctors: Doctor[] =[] ;
+export class HomeComponent implements AfterViewInit, OnInit {
+  imageArray = ['a', 'b', 'c', 'd', 'e', 'f'];
+  searchText = "";
+  doctors: Doctor[] = [];
 
   constructor(private cdr: ChangeDetectorRef, private doctorService: DoctorService) {
     let doctor = new Doctor();
@@ -33,32 +32,42 @@ export class HomeComponent implements AfterViewInit {
     doctor.image = this.getRandomImage();
     this.doctors.push(doctor)
   }
-  
+
+  ngOnInit(): void {
+    this.searchDoctor();
+  }
+
   ngAfterViewInit(): void {
 
     this.cdr.detectChanges();
   }
 
-  getRandomImage(){
+  getRandomImage() {
     let baseUrl = "./../../assets/images/"
-    let randomImage =  Math.floor(Math.random() * this.imageArray.length);
-    return baseUrl+this.imageArray[randomImage]+".jpg";
+    let randomImage = Math.floor(Math.random() * this.imageArray.length);
+    return baseUrl + this.imageArray[randomImage] + ".jpg";
   }
 
   searchDoctor() {
 
-    this.doctorService.searchByText(this.searchText).subscribe(doctors=>{
-      if(doctors) {
-        for(let doc of doctors){
+    this.doctorService.searchByText(this.searchText).subscribe(doctors => {
+      if (doctors) {
+        for (let doc of doctors) {
           doc.image = this.getRandomImage();
         }
         this.cdr.detectChanges();
         this.doctors = doctors;
       }
 
-      console.log('Doctors :::: \n',this.doctors);
-      ;
+      console.log('Doctors :::: \n', this.doctors);
+
     });
+  }
+
+  logout() {
+    sessionStorage.removeItem('tkn');
+    sessionStorage.removeItem('rtkn');
+    window.location.reload();
   }
 
 }
